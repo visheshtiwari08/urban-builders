58b3432e4de7a01b0fd8d94f1e39f1bba2277300
visheshtiwari08 <visheshtiwariindia@gmail.com>
2026-02-01 16:40:41 +0530

    Add process and testimonials sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import TrustBar from "@/components/TrustBar";
 import Services from "@/components/Services";
+import Process from "@/components/Process";
 import Projects from "@/components/Projects";
-import Contact from "../components/Contact";
-
+import Testimonials from "@/components/Testimonials";
 import MapSection from "@/components/MapSection";
+import Contact from "@/components/Contact";
 import FloatingCTA from "@/components/FloatingCTA";
 
 export default function Home() {
@@ -15,7 +16,9 @@ export default function Home() {
       <Hero />
       <TrustBar />
       <Services />
+      <Process />
       <Projects />
+      <Testimonials />
       <MapSection />
       <Contact />
       <FloatingCTA />
